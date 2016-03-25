c0b1d2dcab0669cf1c0613d542385144d7429578
Erjan Gavalji <erjan.gavalji@gmail.com>
2016-03-25 15:39:33 +0200

    Run the extraction from the current dir

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ nsdir=../nativescript/
 ./node_modules/typescript/bin/tsc app.ts ./Scripts/typings/node/node.d.ts --target ES5
 
 (cd $nsdir && grunt --leavecomments=true)
-(cd $nsdir && node ../docs-howto-extractor/app.js -noclean -source ./bin/dist/apps/tests -destination ../docs-howto-extractor/__GENERATED_APIREF__/)
+node app.js -noclean -source ../nativescript/bin/dist/apps/tests -destination ./__GENERATED_APIREF__/
 
